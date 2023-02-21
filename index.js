608a5eaccfccd22c9ab0f7d5fd3e4a05ea45140f
Jinalr <jinal.raval@xceltec.in>
2023-02-21 11:59:11 +0530

    dfdfef

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ mongoose.connect('mongodb+srv://rockeyparekh007:7623838835@cluster0.hsf2kbf.mong
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: ["http://localhost:4200","*"],
+    origin: ["http://codeclick.epizy.com","*"],
     credentials: true,
 })
 );
 
 app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "http://codeclick.epizy.com");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id,authorization");
   
